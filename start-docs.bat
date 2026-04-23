216b18701050b6daa97fc11c6868f93a7f23b72a
ImMalloy <lefehanims@gmail.com>
2026-04-22 19:10:27 -0600

    Revert "uhhhhh"
    This reverts commit aedcbc468d49b98ee91b3b8b4449c7a52f6c437f.

--- start-docs.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-echo Installing dependencies if needed...
-call npm.cmd install
-if errorlevel 1 exit /b %errorlevel%
-echo Starting local docs site at http://localhost:3000/GamebananaAPI-Docs/
-call npm.cmd run start
